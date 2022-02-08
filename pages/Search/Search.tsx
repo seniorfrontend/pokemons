da0843fce27ba428ebab78e91424127f9bd294dd
Eugene <eugenewalknut@gmail.com>
2022-02-08 19:54:50 +0300

    usePokemons hook

---
+++ pages/Search/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as Styling from "./search-styling"
+
+function Search() {
+  return <Styling.Page>
+      
+  </Styling.Page>;
+}
+
+export default Search;
---
+++ pages/Search/search-styling.ts
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled"
+
+export const Page = styled.main``
+
+export const Title = styled.h1``
