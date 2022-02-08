22dce0c43c9a559255036ed20c0bec0535a9c888
Eugene <eugenewalknut@gmail.com>
2022-02-08 19:17:39 +0300

    initial

---
+++ styles/GlobalStyles.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Global, css } from '@emotion/react'
+
+const GlobalStyles = () => {
+    return <Global
+        styles={css`
+            
+        `}
+    />
+}
+
+export default GlobalStyles
